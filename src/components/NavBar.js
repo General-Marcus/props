b52c999f340b5d059e62a359bc8ecc332cd42533
General-Marcus <110114321+General-Marcus@users.noreply.github.com>
2022-08-15 16:57:01 +0000

    lesson one

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      NavBar
+      <h1>Welcome to Issa</h1>
+      <nav>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Products</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
